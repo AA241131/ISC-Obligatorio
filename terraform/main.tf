56ed1ac7a638efabf3f6a901a600dd1759f920d3
Andres <AA241131@fi365.ort.edu.uy>
2026-06-24 01:21:47 -0300

    merge

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,28 @@
-﻿module "vpc-module" {
-    source = "./modules/vpc-module"
-    vpc_cidr_block = var.vpc_cidr_block
+﻿# root module
+# Llamar a los módulos de VPC y EC2, pasando las variables necesarias
+
+provider "aws" {
+region = var.region
+}
+
+module "vpc-module" {
+  source = "./modules/vpc"
+  #variables para el modulo
+  vpc_cidr   = "10.0.0.0/16"
+  nombre_vpc = "vpc-obligatorio-isc"
 }
 
 module "ec2-module" {
     source = "./modules/ec2-module"
-    ami = var.ami_input
-    instance_type_input = var.instance_type_input
-    key_name = var.key_name_input
-    name_instance = var.instance_name_input
-    subnet_id_input = module.vpc-module.subnet_id
-    sg_id_input = module.vpc-module.sg_id
+    instance_ami = "ami-08f44e8eca9095668"
+    instance_type = "t3.micro"
+    instance_iam_profile = "LabInstanceProfile"
+    key_name = "vockey"
+    instance_name = "webserver1"
+    instance_subnet = module.vpc-module.subnet-publica-id
+    instance_security_groups = module.vpc-module.sg-instancias-id
 }
 
+output "dns-output" {
+    value = module.ec2-module.dns
+}
