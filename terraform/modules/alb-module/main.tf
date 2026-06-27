41e1f373bafa5c1b42f131a08b5cb474b39ffb3f
Andres <AA241131@fi365.ort.edu.uy>
2026-06-27 15:38:05 -0300

    target group nombre fix

--- terraform/modules/alb-module/main.tf
+++ terraform/modules/alb-module/main.tf
@@ -22,7 +22,7 @@ resource "aws_lb_listener" "ALB_listener" {
 }
 
 resource "aws_lb_target_group" "target_group" {
-  name     = "obligatorio-isc-target-group"
+  name_prefix = "target-group-isc"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
