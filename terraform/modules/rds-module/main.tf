37b32f783b0513b3d8fd411fc68249059820157d
Andres <AA241131@fi365.ort.edu.uy>
2026-06-27 16:08:34 -0300

    rds back up

--- terraform/modules/rds-module/main.tf
+++ terraform/modules/rds-module/main.tf
@@ -19,10 +19,23 @@ resource "aws_db_instance" "APPBD" {
   port                 = 3306
   parameter_group_name = "default.mysql8.0"
   publicly_accessible    = false
-  skip_final_snapshot  = true
   multi_az              = false
   vpc_security_group_ids = var.sg_id_input
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+
+  # Backups
+  backup_retention_period = 7
+  backup_window           = "03:00-04:00"
+
+  # ventana de mantenimiento
+  maintenance_window = "Sun:04:00-Sun:05:00"
+
+  # comportamiento de borrado
+  deletion_protection = false
+  skip_final_snapshot = false
+  final_snapshot_identifier = "ecommerce-final-snapshot"
+
+    
   tags = {
     Name = "rds-ecommerce-instance"
   }
